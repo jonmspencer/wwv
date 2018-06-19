d75e6c51b238a4551c616de4a1fc1a10f738be65
Jon M Spencer <jon@jonspencer.us>
2018-06-19 01:07:45 -0400

    Fix the youtube player embed

--- wp-content/themes/bad/inc/functions/views/post-views.php
+++ wp-content/themes/bad/inc/functions/views/post-views.php
@@ -3,7 +3,13 @@
   // Single Post
   function render_single_post() {
     get_header();
-    // get_template_part("shared/hero");
+    if(have_posts()) {
+      while (have_posts()) : the_post();
+        $youtube_video_id = get_post_meta( get_the_ID(), 'post_youtube_video_id', true );
+        include(locate_template("shared/video/player.php"));
+        unset($youtube_video_id);
+      endwhile;
+    }
 
 
     get_footer();
---
+++ wp-content/themes/bad/shared/video/player.php
@@ -0,0 +1,3 @@
+<div class="iframe-container">
+  <iframe src="https://www.youtube-nocookie.com/embed/<?php echo $youtube_video_id; ?>?modestbranding=0&autohide=0&showinfo=0&ecver=2&enablejsapi=1&color=white&rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+</div>
