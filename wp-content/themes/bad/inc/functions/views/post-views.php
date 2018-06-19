776cecc9f0215f77601691334ce5cf9c55d19813
Jon M Spencer <jon@jonspencer.us>
2018-06-19 01:13:18 -0400

    Add base single structure

--- wp-content/themes/bad/inc/functions/views/post-views.php
+++ wp-content/themes/bad/inc/functions/views/post-views.php
@@ -6,7 +6,7 @@
     if(have_posts()) {
       while (have_posts()) : the_post();
         $youtube_video_id = get_post_meta( get_the_ID(), 'post_youtube_video_id', true );
-        include(locate_template("shared/video/player.php"));
+        include(locate_template("shared/video/video-layout.php"));
         unset($youtube_video_id);
       endwhile;
     }
---
+++ wp-content/themes/bad/shared/video/video-layout.php
@@ -0,0 +1,9 @@
+<div class="wrap video-layout">
+  <?php include(locate_template("shared/video/player.php")); ?>
+  <div class="video-layout-content">
+    <h1><?php the_title(); ?></h1>
+  </div>
+  <div class="video-layout-related">
+
+  </div>
+</div>
